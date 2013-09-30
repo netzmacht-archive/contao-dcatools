f796f2196f5461be099b84c3ae57d8400305cd40
David Molineus <mail@netzmacht.de>
2013-09-30 18:15:15 +0200

    start unit testing

---
+++ tests/DcaToolsTest.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: david
+ * Date: 30.09.13
+ * Time: 18:09
+ * To change this template use File | Settings | File Templates.
+ */
+
+require_once dirname(dirname(__FILE__)) . ''
+
+class DcaToolsTest {
+
+}

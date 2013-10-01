747089fad17ae61ec470a804fd3947c980401b1a
David Molineus <mail@netzmacht.de>
2013-10-01 16:13:55 +0200

    unit testing

--- tests/DcaToolsTest.php
+++ tests/DcaToolsTest.php
@@ -7,9 +7,74 @@
  * To change this template use File | Settings | File Templates.
  */
 
-require_once dirname(dirname(__FILE__)) . 'DcaTools/';
+require_once dirname(dirname(__FILE__)) . 'src/system/modules/dcatools';
 
-class DcaToolsTest
+$GLOBALS['TL_DCA']['tl_test'] = array();
+
+
+class DcaToolsTest extends PHPUnit_Framework_TestCase
 {
+	/**
+	 * @dataProvider provideDoAutoUpdate
+	 */
+	public function testDoAutoUpdate($value, $expected)
+	{
+		$actual = \Netzmacht\DcaTools\DcaTools::doAutoUpdate($value);
+		$this->assertEquals($expected, $actual);
+	}
+
+	public function provideDoAutoUpdate()
+	{
+		return array
+		(
+			array(null, false),
+			array(false, false),
+			array(true, true),
+		);
+	}
+
+
+	/**
+	 * @dataProvider provideGetDataContainer
+	 * @param $value
+	 * @param $expected
+	 */
+	public function testGetDataContainer($value, $expected)
+	{
+		$objDc = \Netzmacht\DcaTools\DcaTools::getDataContainer('tl_test');
+		$this->assertEquals($objDc, $expected);
+	}
+
+	public function provideGetDataContainer()
+	{
+		return array
+		(
+			array('tl_test', new \Netzmacht\DcaTools\DataContainer('tl_test'))
+		);
+	}
+
+
+	public function testHookLoadDataContainer()
+	{
+		$objTools = new \Netzmacht\DcaTools\DcaTools();
+		$objTools->hookLoadDataContainer('tl_test');
+
+		$this->assertEquals(
+			$GLOBALS['TL_DCA']['tl_test'],
+			array()
+		);
+
+		$GLOBALS['TL_DCA']['tl_test']['dcatools'] = array();
+
+		$this->assertEquals(
+			$GLOBALS['TL_DCA']['tl_test'],
+			array(
+				'dcatools' => array(),
+				'config' => array(
+					'onload_callback' => array('Netzmacht\DcaTools\DcaTools', 'initializeDataContainer')
+				),
+			)
+		);
+	}
 
 }

fb4c36446cded98d3c483b75ee1d71b8145993b7
David Molineus <mail@netzmacht.de>
2013-10-10 11:15:32 +0200

    remove ArrayAccess of Property class

--- src/system/modules/dcatools/DcaTools/Property.php
+++ src/system/modules/dcatools/DcaTools/Property.php
@@ -23,7 +23,7 @@ use Netzmacht\DcaTools\Palette\SubPalette;
  * Class Property
  * @package Netzmacht\DcaTools\Palette
  */
-class Property extends Child implements \ArrayAccess, PropertyInterface
+class Property extends Child implements PropertyInterface
 {
 	/**
 	 * @var PropertyContainer
@@ -43,72 +43,6 @@ class Property extends Child implements \ArrayAccess, PropertyInterface
 		parent::__construct($strName, $objDataContainer, $arrDefinition['propertys'][$strName]);
 
 		$this->objParent = $objParent === null ? $objDataContainer : $objParent;
-	}
-
-
-	/**
-	 * (PHP 5 &gt;= 5.0.0)<br/>
-	 * Whether a offset exists
-	 * @link http://php.net/manual/en/arrayaccess.offsetexists.php
-	 * @param mixed $offset <p>
-	 * An offset to check for.
-	 * </p>
-	 * @return boolean true on success or false on failure.
-	 * </p>
-	 * <p>
-	 * The return value will be casted to boolean if non-boolean was returned.
-	 */
-	public function offsetExists($offset)
-	{
-		return array_key_exists($offset, $this->definition);
-	}
-
-
-	/**
-	 * (PHP 5 &gt;= 5.0.0)<br/>
-	 * Offset to retrieve
-	 * @link http://php.net/manual/en/arrayaccess.offsetget.php
-	 * @param mixed $offset <p>
-	 * The offset to retrieve.
-	 * </p>
-	 * @return mixed Can return all value types.
-	 */
-	public function &offsetGet($offset)
-	{
-		return $this->definition[$offset];
-	}
-
-
-	/**
-	 * (PHP 5 &gt;= 5.0.0)<br/>
-	 * Offset to set
-	 * @link http://php.net/manual/en/arrayaccess.offsetset.php
-	 * @param mixed $offset <p>
-	 * The offset to assign the value to.
-	 * </p>
-	 * @param mixed $value <p>
-	 * The value to set.
-	 * </p>
-	 * @return void
-	 */
-	public function offsetSet($offset, $value)
-	{
-		$this->definition[$offset] = $value;
-	}
-
-
-	/**
-	 * (PHP 5 &gt;= 5.0.0)<br/>
-	 * Offset to unset
-	 * @link http://php.net/manual/en/arrayaccess.offsetunset.php
-	 * @param mixed $offset <p>
-	 * The offset to unset.
-	 * </p>
-	 * @return void
-	 */
-	public function offsetUnset($offset)
-	{
-		unset($this->definition[$offset]);
 	}
 
 

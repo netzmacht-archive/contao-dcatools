a8884f7a621d3d13d3b58072510ee473eec21e43
David Molineus <mail@netzmacht.de>
2013-11-19 12:04:24 +0100

    move events to config.php

--- src/system/modules/dcatools/config/config.php
+++ src/system/modules/dcatools/config/config.php
@@ -2,3 +2,14 @@
 
 
 $GLOBALS['TL_HOOKS']['loadDataContainer'][] = array('DcaTools\Bridge', 'hookLoadDataContainer');
+
+
+/**
+ * events
+ */
+$GLOBALS['TL_EVENTS']['dcatools.tl_content.getAllowedEntries'][]         = array('DcaTools\Dca\Content', 'getAllowedEntries');
+$GLOBALS['TL_EVENTS']['dcatools.tl_content.getAllowedIds'][]             = array('DcaTools\Dca\Content', 'getAllowedIds');
+$GLOBALS['TL_EVENTS']['dcatools.tl_content.getAllowedDynamicParents'][]  = array('DcaTools\Dca\Content', 'getAllowedDynamicParents');
+
+
+$GLOBALS['TL_EVENTS']['dcatools.tl_content.getDynamicParent'][] = array('DcaTools\Dca\Content', 'getParentName');

7320be7cf919ec84829370b53c5ca85546c8f78f
David Molineus <mail@netzmacht.de>
2014-06-24 08:59:58 +0200

    add assertion class

---
+++ src/DcaTools/Assertion.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @package    dev
+ * @author     David Molineus <david.molineus@netzmacht.de>
+ * @copyright  2014 netzmacht creative David Molineus
+ * @license    LGPL 3.0
+ * @filesource
+ *
+ */
+
+namespace DcaTools;
+
+
+class Assertion extends \Assert\Assertion
+{
+	protected static $exceptionClass = 'DcaTools\Exception\InvalidArgumentException';
+
+} 
